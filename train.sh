52160dd32a6282c015d92283a63edc8bc382edac
linchenxi <chenxi.lin.2016@gmail.com>
2024-12-06 13:55:46 -0800

    **

---
+++ train.sh
@@ -0,0 +1,6 @@
+python main.py \
+--coco_path "/NAS3/Members/linchenxi/projects/datasets" \
+--model_name "DETR_1" \
+--batch_size 24 \
+--output_dir "/NAS3/Members/linchenxi/projects/object_detection/detr"
+
